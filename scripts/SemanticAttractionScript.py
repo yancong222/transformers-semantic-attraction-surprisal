871e2380cf08efaf566ca56c7be30e10f27e327a
Yan Cong <52762906+yancong222@users.noreply.github.com>
2023-05-26 12:28:59 -0400

    Update SemanticAttractionScript.py

--- scripts/SemanticAttractionScript.py
+++ scripts/SemanticAttractionScript.py
@@ -1,19 +1,14 @@
 !pip install minicons
 import pandas as pd
 import numpy as np
-import re
-import math
-import os
-import string
-import shutil, sys, glob
+import torch
 from minicons import scorer
 
-la = pd.read_csv('laurinavichyute_dataset_minicons.csv', index_col = 0)
 cn = pd.read_csv('cunnings_dataset_dataset_minicons.csv', index_col = 0)
 
-ilm_model = scorer.IncrementalLMScorer('gpt2', 'cpu') 
-distilgpt2 = scorer.IncrementalLMScorer('distilgpt2', 'cpu')
-gpt_neo = scorer.IncrementalLMScorer('EleutherAI/gpt-neo-1.3B', 'cpu')
+ilm_model = scorer.IncrementalLMScorer('gpt2', 'cuda') 
+distilgpt2 = scorer.IncrementalLMScorer('distilgpt2', 'cuda')
+gpt_neo = scorer.IncrementalLMScorer('EleutherAI/gpt-neo-1.3B', 'cuda')
 
 def ilm_sent_mean_surp(stimuli):
   lst = []
@@ -33,9 +28,6 @@ def distilgpt2_sent_mean_surp(stimuli):
   score = distilgpt2.sequence_score(lst, reduction = lambda x: -x.mean(0).item())
   return round(score[0], 2)
 
-la['ilm_sent_mean_surp'] = la['Sentence'].apply(lambda x: ilm_sent_mean_surp(x))
-la['distilgpt2_sent_mean_surp'] = la['Sentence'].apply(lambda x: distilgpt2_sent_mean_surp(x))
-la['gptneo_sent_mean_surp'] = la['Sentence'].apply(lambda x: gptneo_sent_mean_surp(x))
 cn['ilm_sent_mean_surp'] = cn['Sentence'].apply(lambda x: ilm_sent_mean_surp(x))
 cn['distilgpt2_sent_mean_surp'] = cn['Sentence'].apply(lambda x: distilgpt2_sent_mean_surp(x))
 cn['gptneo_sent_mean_surp'] = cn['Sentence'].apply(lambda x: gptneo_sent_mean_surp(x))
@@ -50,9 +42,6 @@ def target_surp(model, sentence, target):
     result = [y[1] for x, y in enumerate(tuple_list) if y[0] in target]
   return round(np.nanmean(result), 2)
 
-la['ilm_target_mean_surp'] = la.apply(lambda x: target_surp(ilm_model, x.Sentence, x.Target), axis=1)
-la['distilgpt2_target_mean_surp'] = la.apply(lambda x: target_surp(distilgpt2, x.Sentence, x.Target), axis=1)
-la['gptneo_target_mean_surp'] = la.apply(lambda x: target_surp(gpt_neo, x.Sentence, x.Target), axis=1)
 cn['ilm_target_mean_surp'] = cn.apply(lambda x: target_surp(ilm_model, x.Sentence, x.Target), axis = 1)
 cn['distilgpt2_target_mean_surp'] = cn.apply(lambda x: target_surp(distilgpt2, x.Sentence, x.Target), axis=1)
 cn['gptneo_target_mean_surp'] = cn.apply(lambda x: target_surp(gpt_neo, x.Sentence, x.Target), axis=1)
@@ -64,11 +53,6 @@ def get_tokens_len_score(sent, model):
   result = []
   result.append([len(output[0]), output[0]])
   return result
-
-models = [('ilm', gpt2), ('distilgpt2', distilgpt2), ('gptneo', gpt_neo)]
-for model in models:
-  la[model[0] + '_len_tokens'] = la['Sentence'].apply(lambda x: get_tokens_len_score(x, model[1])[0][0])
-  la[model[0] + '_tokens_surpscore'] = la['Sentence'].apply(lambda x: get_tokens_len_score(x, model[1])[0][1])
   
 models = [('ilm', gpt2), ('distilgpt2', distilgpt2), ('gptneo', gpt_neo)]
 for model in models:
